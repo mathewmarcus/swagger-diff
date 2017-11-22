477e75367514d426a7cb96b21dc2f7217e3805da
mathewmarcus <mathewmarcus456@gmail.com>
2017-11-21 23:57:29 -0600

    add update_errors docstring

--- swagger_diff/errors.py
+++ swagger_diff/errors.py
@@ -3,8 +3,31 @@ _errors = {}
 
 
 def update_errors(canonical_key, message):
+    """ Update the global errors dict with a new error
+
+    Args:
+       canonical_key (str): canonical key path in the errors dict
+       message (str): error message
+
+    Returns:
+        None
+
+    Examples:
+        >>> from swagger_diff import errors
+        >>> print(errors)
+        {}
+        >>> update_errors('/hello/world/foo/bar', 'Some error message')
+        None
+        >>> print(errors)
+        {'hello': {'world': {'foo': {'bar': 'Some error message'}}}}
+
+    """
+
     global _errors
-    keys = canonical_key.split('/')
+    keys = canonical_key.lstrip('/').split('/')
+    new_errors = _errors
+
     for key in keys[:-1]:
-        _errors = _errors.setdefault(key, {})
-    _errors[keys[-1]] = message
+        new_errors = new_errors.setdefault(key, {})
+
+    new_errors[keys[-1]] = message
--- test/integration/test_errors.py
+++ test/integration/test_errors.py
@@ -3,4 +3,5 @@ from swagger_diff.errors import update_errors, _errors
 
 def test_errors():
     update_errors('hello/world/foo/bar', 'Some error message')
-    assert {'hello': {'world': {'foo': {'bar': 'Some error message'}}}} == _errors
+    update_errors('hello/world/foo/baz', 'Another error message')
+    assert {'hello': {'world': {'foo': {'bar': 'Some error message', 'baz': 'Another error message'}}}} == _errors
